c35bf758235b276dd235ccaf9802ad1adbe3901a
phamnguyenh37h-ai <phamnguyenh37h@gmail.com>
2026-04-22 20:35:33 +0700

    Add script.js file with JavaScript code for portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript code for portfolio
+
+// Function to display greeting
+function displayGreeting() {
+    const greeting = 'Hello, welcome to my portfolio!';
+    console.log(greeting);
+}
+
+displayGreeting();
